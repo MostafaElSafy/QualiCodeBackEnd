b1737a9a7e6be555010873093dbc71172e920283
AE307 <a_elsayed_f@icloud.com>
2024-01-15 19:01:46 +0200

    updateast

--- AST.py
+++ AST.py
@@ -6,39 +6,52 @@ def count_nested_structures(code):
     class StructureVisitor(ast.NodeVisitor):
         def __init__(self):
             self.nested_loops = 0
-            self.nested_ifs = 0
-            self.nested_whiles = 0
-            self.nested_fors = 0
+            self.nested_ifs_inside_loops = 0
+            self.nested_ifs_outside_loops = 0
+            self.for_loops_count = 0
+            self.while_loops_count = 0
             self.current_depth = 0
+            self.is_outer_loop = True
 
         def visit_For(self, node):
+            if self.is_outer_loop:
+                self.nested_loops += 1
+                self.is_outer_loop = False
+
+            self.for_loops_count += 1
             self.current_depth += 1
-            self.nested_loops = max(self.nested_loops, self.current_depth)
-            self.nested_fors += 1
             self.generic_visit(node)
             self.current_depth -= 1
 
         def visit_While(self, node):
+            self.nested_loops += 1
+            self.while_loops_count += 1
             self.current_depth += 1
-            self.nested_loops = max(self.nested_loops, self.current_depth)
-            self.nested_whiles += 1
             self.generic_visit(node)
             self.current_depth -= 1
 
         def visit_If(self, node):
+            if self.current_depth > 0:
+                self.nested_ifs_inside_loops += 1
+            else:
+                self.nested_ifs_outside_loops += 1
             self.current_depth += 1
-            self.nested_ifs = max(self.nested_ifs, self.current_depth)
             self.generic_visit(node)
             self.current_depth -= 1
+
+        def visit_Else(self, node):
+            # Additional handling for the 'else' clause
+            self.generic_visit(node)
 
     visitor = StructureVisitor()
     visitor.visit(tree)
     
     return {
         'nested_loops': visitor.nested_loops,
-        'nested_ifs': visitor.nested_ifs,
-        'nested_whiles': visitor.nested_whiles,
-        'nested_fors': visitor.nested_fors
+        'nested_ifs_inside_loops': visitor.nested_ifs_inside_loops,
+        'nested_ifs_outside_loops': visitor.nested_ifs_outside_loops,
+        'for_loops_count': visitor.for_loops_count,
+        'while_loops_count': visitor.while_loops_count,
     }
 
 # Example usage
@@ -60,6 +73,6 @@ else:
 
 result = count_nested_structures(code_example)
 print("Number of nested loops:", result['nested_loops'])
-print("Number of nested ifs:", result['nested_ifs'])
-print("Number of nested while loops:", result['nested_whiles'])
-print("Number of nested for loops:", result['nested_fors'])
+print("Number of 'if' conditions:", result['nested_ifs_inside_loops']+result['nested_ifs_outside_loops'])
+print("Number of 'for' loops:", result['for_loops_count'])
+print("Number of 'while' loops:", result['while_loops_count'])
